fb12f57240b9e53c1770263b1f91dd0025560e46
Satoru Osawa <s.osawa29@gmail.com>
2017-10-13 17:55:21 +0900

    list -> vector

--- src/BookShelf.java
+++ src/BookShelf.java
@@ -1,14 +1,13 @@
+import java.util.Vector;
 public class BookShelf implements Aggregate {
-	private Book[] books;
+	private Vector<Book> books = new Vector<Book>();
 	private int last = 0;
-	public BookShelf(int maxsize) {
-		this.books = new Book[maxsize];
-	}
+	public BookShelf() {}
 	public Book getBookAt (int index) {
-		return books[index];
+		return books.get(index);
 	}
 	public void appendBook(Book book) {
-		this.books[last] = book;
+		this.books.add(book);
 		last++;
 	}
 	public int getLength() {
